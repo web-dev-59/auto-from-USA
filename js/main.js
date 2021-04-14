166fc8b5f6f1904423902645e796c9d053be2f14
Deniskot59 <63236749+Deniskot59@users.noreply.github.com>
2021-04-14 14:37:11 +0300

    слайдер готов

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
+$(function () {
+  $('.carousel__inner').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
